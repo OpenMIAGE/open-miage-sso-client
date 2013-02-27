1e3de7ade5fc1e3a3b40f042ca74b515cd7128e4
gsaunier <gael.saunier@open-miage.fr>
2013-02-27 23:40:43 +0100

    fix file tree rework

--- example/lib.php
+++ example/lib.php
@@ -1,4 +1,11 @@
 <?php
 
-Import::addLibPath("openid/2.0.2");
+if (is_dir(dirname(__DIR__) . "/src"))
+    Import::addClassPath(dirname(__DIR__) . "/src");
+else
+    Import::addLibPath("OpenM-SSO/client/1.0.3");
+Import::addLibPath("openid/2.0.2x");
+Import::addLibPath("OpenM-Commons/api/1.0.0");
+Import::addLibPath("OpenM-SSO/api/1.0.0");
+Import::addLibPath("OpenM-ID/api/1.0.0");
 ?>
--- example/src.php
+++ example/src.php
@@ -1,15 +1,22 @@
 <?php
-if(!defined("OpenM_Import")){
-    if(is_file(dirname(dirname(dirname(dirname(__FILE__)))). '/src/Import.class.php'))
-        require_once dirname(dirname(dirname(dirname(__FILE__)))). '/src/Import.class.php';
-    else if(is_file("../OpenM-SSO.path"))
-        require_once dirname(dirname(((__FILE__)))). '/lib/'.  file_get_contents("../OpenM-SSO.path").'/Import.class.php';
-    else if(is_file("../../OpenM-SSO.path"))
-        require_once dirname(dirname(((__FILE__)))). '/lib/'.  file_get_contents("../../OpenM-SSO.path").'/Import.class.php';
+
+if (!class_exists("Import")) {
+    if (is_file(__DIR__ . "/OpenM.util.version"))
+        $version = file_get_contents(__DIR__ . "/OpenM.util.version");
+    else if (is_file(dirname(__DIR__) . "/lib/OpenM.util.version"))
+        $version = file_get_contents(dirname(__DIR__) . "/lib/OpenM.util.version");
     else
-        throw new Exception("OpenM-SSO.client not found");
-    Import::addClassPath(dirname(__FILE__));
-    define("OpenM_Import", true);
+        throw new Exception("lib OpenM.util version not found");
+
+    $utilVersion = explode("=", $version);
+    $utilVersion = $utilVersion[0];
+    $util_suffix = "/lib/$utilVersion/Import.class.php";
+    if (is_file(dirname(dirname(((__FILE__)))) . $util_suffix))
+        require_once dirname(dirname(((__FILE__)))) . $util_suffix;
+    else if (is_file(dirname(dirname(dirname(__FILE__))) . $util_suffix))
+        require_once dirname(dirname(dirname(__FILE__))) . $util_suffix;
+    else
+        throw new Exception("lib $utilVersion not found");
 }
 
 require_once 'lib.php';
