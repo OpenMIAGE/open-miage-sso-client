ec71a5eca597cc4b8c3298b5cb95c20dab29fb9d
gsaunier <gael.saunier@open-miage.fr>
2013-05-11 14:56:01 +0200

    1.0.5 RC3

--- src/OpenM-ID/client/OpenM_IDLoginClientServer.class.php
+++ src/OpenM-ID/client/OpenM_IDLoginClientServer.class.php
@@ -12,23 +12,91 @@ class OpenM_IDLoginClientServer {
     private $config_file_path;
     private $embeded;
 
-    const IS_CONNECTED_PARAMETER = "isConnected";
-    const RECONNECT = "reconnect";
+    const SESSION_MODE = "OpenM_IDLoginClientServer_session_mode";
+    const MODE_PARAMETER = "API_SESSION_MODE";
+    const API_SELECTION_PARAMETER = "API";
+    const MODE_API_SELECTION = "API_SELECTION";
+    const MODE_ALL_API = "ALL_API";
+    const MODE_WITHOUT_API = "WITHOUT_API";
+    const SESSION_API_SELECTED = "OpenM_IDLoginClientServer_api_selected";
+    const ACTION_PARAMETER = "ACTION";
+    const IS_CONNECTED_ACTION = "isConnected";
+    const RETURN_IS_CONNECTED_PARAMETER = self::IS_CONNECTED_ACTION;
 
-    public function __construct($config_file_path, $embede = true) {
+    public function __construct($config_file_path, $embeded = true) {
         $this->config_file_path = $config_file_path;
-        $this->embeded = $embede;
+        $this->embeded = $embeded;
     }
 
     public function handle() {
         $manager = OpenM_SSOClientPoolSessionManager::fromFile($this->config_file_path);
-        $sso = $manager->get();
-        if (isset($_GET[self::IS_CONNECTED_PARAMETER]))
-            die("{\"" . self::IS_CONNECTED_PARAMETER . "\":" . ($sso->isConnected() ? 1 : 0) . "}");
+        $mode = OpenM_SessionController::get(self::SESSION_MODE);
+        $api = OpenM_SessionController::get(self::SESSION_API_SELECTED);
+
+        if ($mode === null) {
+            if (isset($_GET[self::MODE_PARAMETER])) {
+                switch ($_GET[self::MODE_PARAMETER]) {
+                    case self::MODE_API_SELECTION:
+                        $mode = self::MODE_API_SELECTION;
+                        if (isset($_GET[self::API_SELECTION_PARAMETER])) {
+                            $api = $_GET[self::API_SELECTION_PARAMETER];
+                            OpenM_SessionController::set(self::SESSION_API_SELECTED, $api);
+                        } else {
+                            die("no api selected");
+                        }
+                        break;
+                    case self::MODE_ALL_API:
+                        $mode = self::MODE_ALL_API;
+                        break;
+                    case self::MODE_WITHOUT_API:
+                        $mode = self::MODE_WITHOUT_API;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if ($mode !== null)
+                OpenM_SessionController::set(self::SESSION_MODE, $mode);
+        }
+
+        $action = null;
+        if (isset($_POST[self::ACTION_PARAMETER]) && $_POST[self::ACTION_PARAMETER] == self::IS_CONNECTED_ACTION)
+            $action = self::IS_CONNECTED_ACTION;
+
+        switch ($mode) {
+            case self::MODE_API_SELECTION:
+                $sso = $manager->get(OpenM_SessionController::get(self::SESSION_API_SELECTED), false);
+                break;
+            case self::MODE_ALL_API:
+                die("not implemented for now");
+                break;
+            case self::MODE_WITHOUT_API:
+                $sso = $manager->get();
+                break;
+            default:
+                die("no mode selected");
+                break;
+        }
 
         if ($this->embeded)
             $sso->setEmbeded();
-        $sso->login(null, isset($_GET[self::RECONNECT]));
+
+        switch ($action) {
+            case self::IS_CONNECTED_ACTION:
+                $this->isConnected($sso);
+                break;
+            default:
+                $sso->login(null, true);
+                $this->isConnectedDisplay($sso);
+                break;
+        }
+    }
+
+    public function isConnected($sso) {
+        die("{\"" . self::RETURN_IS_CONNECTED_PARAMETER . "\":" . ($sso->isConnected() ? 1 : 0) . "}");
+    }
+
+    public function isConnectedDisplay($sso) {
         if ($sso->isConnected())
             echo "you're connected";
         else
--- src/OpenM-ID/gui/js/OpenM_IDLoginClient.js
+++ src/OpenM-ID/gui/js/OpenM_IDLoginClient.js
@@ -1,13 +1,25 @@
 var OpenM_IDLoginClient = {
     'url': '',    
+    'session_mode': '',
+    'api_selected': '',
+    'alreadyHaveConnectionOK': false,
+    'MODE_PARAMETER': "API_SESSION_MODE",
+    'API_SELECTION_PARAMETER' : "API",
+    'MODE_API_SELECTION' : "API_SELECTION",
+    'MODE_ALL_API' : "ALL_API",
+    'MODE_WITHOUT_API' : "WITHOUT_API",
+    'ACTION_PARAMETER' : "ACTION",
+    'IS_CONNECTED_ACTION' : "isConnected",
+    'RETURN_IS_CONNECTED_PARAMETER' : "isConnected",
     'connected': false,
     'frame': undefined,
     'open': function(){
         if(this.connected)
             return;
+        this.close();
         this.frame = $(document.createElement('div')).attr("id",'OpenM_IDLoginClient-frame');
         this.frame.addClass("hero-unit OpenM_IDLoginClient-frame");
-        var iframe = $(document.createElement("iframe")).attr("id", 'OpenM_IDLoginClient-iframe').attr("src", this.url);
+        var iframe = $(document.createElement("iframe")).attr("id", 'OpenM_IDLoginClient-iframe').attr("src", this.url+"?"+this.MODE_PARAMETER+"="+this.session_mode+"&"+this.API_SELECTION_PARAMETER+"="+this.api_selected);
         var close = $(document.createElement("button")).attr("type", "button").addClass("close").attr("data-dismiss","modal").attr("aria-hidden", true).append("&times;");
         close.addClass("OpenM_IDLoginClient-close");
         close.attr("onclick", "OpenM_IDLoginClient.close();return false;");
@@ -23,9 +35,11 @@ var OpenM_IDLoginClient = {
     },
     'reconnectframe' : undefined,
     'reconnect': function(callback_function){
+        if(!this.alreadyHaveConnectionOK)
+            return;
         this.connected = false;
         this.reconnectframe = $(document.createElement("iframe"));
-        this.reconnectframe.attr("src", this.url+"?reconnect");
+        this.reconnectframe.attr("src", this.url+"?"+this.MODE_PARAMETER+"="+this.session_mode+"&"+this.API_SELECTION_PARAMETER+"="+this.api_selected);
         this.reconnectframe.addClass("OpenM_IDLoginClient-reconnectframe");
         $("html body").append(this.reconnectframe);
         var controller = this;
@@ -37,9 +51,12 @@ var OpenM_IDLoginClient = {
         });
     },
     'close': function(){
-        this.frame.remove();
+        if(this.frame!=undefined)
+            this.frame.remove();
+        if(this.timer_daemon!==undefined)
+            window.clearTimeout(this.timer_daemon);
     },
-    'isConnected': function(){
+    'isConnected': function(synchro){
         if(this.connected){
             if(this.frame!=undefined)
                 this.frame.remove();
@@ -47,17 +64,28 @@ var OpenM_IDLoginClient = {
         }
         
         var controller = this;
-        $.get(this.url, {
-            isConnected:''
-        }, function(data){
-            if(data.isConnected!==undefined && data.isConnected==1){
-                controller.connected = true;
-                if(controller.callback_when_connected!==undefined)
-                    controller.callback_when_connected();
-            }
-        }, "json");
+        if(synchro===undefined)
+            synchro = false;
         
-        return false;
+        var ajax = {
+            async: !synchro,
+            type: 'POST', 
+            data: {}, 
+            url: this.url+"?"+this.MODE_PARAMETER+"="+this.session_mode+"&"+this.API_SELECTION_PARAMETER+"="+this.api_selected, 
+            dataType: 'json',
+            success: function(data){
+                if(data[controller.RETURN_IS_CONNECTED_PARAMETER]==1){
+                    controller.connected = true;
+                    controller.alreadyHaveConnectionOK = true;
+                    if(controller.callback_when_connected!==undefined)
+                        controller.callback_when_connected();
+                }
+            }
+        };
+        ajax.data[this.ACTION_PARAMETER] = this.IS_CONNECTED_ACTION;
+        
+        $.ajax(ajax);
+        return this.connected;
     },
     'timer_daemon': null,
     'timer_interval': 500,
