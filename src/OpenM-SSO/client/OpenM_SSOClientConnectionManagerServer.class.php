0d1cf20df74dfd70ec6510280b47a088dfc58831
gsaunier <gael.saunier@open-miage.fr>
2013-07-24 18:28:39 +0200

    add redirection mode to login (not enabled by default)

--- src/OpenM-SSO/client/OpenM_SSOClientConnectionManagerServer.class.php
+++ src/OpenM-SSO/client/OpenM_SSOClientConnectionManagerServer.class.php
@@ -12,6 +12,7 @@ class OpenM_SSOClientConnectionManagerServer {
     private $config_file_path;
     private $embeded;
 
+    const LOGIN_IN_PROGRESS = "OpenM_IDLoginClientServer_login_in_progress";
     const SESSION_MODE = "OpenM_IDLoginClientServer_session_mode";
     const MODE_PARAMETER = "API_SESSION_MODE";
     const API_SELECTION_PARAMETER = "API";
@@ -22,6 +23,9 @@ class OpenM_SSOClientConnectionManagerServer {
     const ACTION_PARAMETER = "ACTION";
     const IS_CONNECTED_ACTION = "isConnected";
     const RETURN_IS_CONNECTED_PARAMETER = self::IS_CONNECTED_ACTION;
+    const LOGIN_ACTION = "login";
+    const RETURN_TO_PARAMETER = "proxy_return_to";
+    const DASH = "! !";
 
     public function __construct($config_file_path, $embeded = true) {
         OpenM_Log::debug("server initialisation", __CLASS__, __METHOD__, __LINE__);
@@ -30,11 +34,16 @@ class OpenM_SSOClientConnectionManagerServer {
     }
 
     public function handle() {
+
+        if (OpenM_SessionController::get(self::LOGIN_IN_PROGRESS) === true)
+            return $this->loginInProgress();
+
         $manager = OpenM_SSOClientPoolSessionManager::fromFile($this->config_file_path);
         $mode = OpenM_SessionController::get(self::SESSION_MODE);
         OpenM_Log::debug("mode[session]: $mode", __CLASS__, __METHOD__, __LINE__);
         $api = OpenM_SessionController::get(self::SESSION_API_SELECTED);
         OpenM_Log::debug("api[session]: $api", __CLASS__, __METHOD__, __LINE__);
+
         if ($mode === null) {
             if (isset($_GET[self::MODE_PARAMETER])) {
                 switch ($_GET[self::MODE_PARAMETER]) {
@@ -57,14 +66,18 @@ class OpenM_SSOClientConnectionManagerServer {
                         break;
                 }
             }
-            if ($mode !== null)
-                OpenM_SessionController::set(self::SESSION_MODE, $mode);
         }
+
+        if ($mode !== null)
+            OpenM_SessionController::set(self::SESSION_MODE, $mode);
+
         OpenM_Log::debug("api:$api, mode:$mode", __CLASS__, __METHOD__, __LINE__);
 
         $action = null;
-        if (isset($_POST[self::ACTION_PARAMETER]) && $_POST[self::ACTION_PARAMETER] == self::IS_CONNECTED_ACTION)
-            $action = self::IS_CONNECTED_ACTION;
+        if (isset($_POST[self::ACTION_PARAMETER]))
+            $action = $_POST[self::ACTION_PARAMETER];
+        else if (isset($_GET[self::ACTION_PARAMETER]))
+            $action = $_GET[self::ACTION_PARAMETER];
         OpenM_Log::debug("action:$action", __CLASS__, __METHOD__, __LINE__);
 
         switch ($mode) {
@@ -92,12 +105,38 @@ class OpenM_SSOClientConnectionManagerServer {
                 OpenM_Log::debug("isConnected", __CLASS__, __METHOD__, __LINE__);
                 $this->isConnected($sso);
                 break;
+            case self::LOGIN_ACTION:
+                OpenM_Log::debug("login", __CLASS__, __METHOD__, __LINE__);
+                if ($sso->isConnected())
+                    $this->return_to();
+                else {
+                    OpenM_SessionController::set(self::LOGIN_IN_PROGRESS, true);
+                    return $this->loginInProgress();
+                }
+                break;
             default:
                 OpenM_Log::debug("action:login(null, true)", __CLASS__, __METHOD__, __LINE__);
                 $sso->login(null, true);
                 $this->isConnectedDisplay($sso);
                 break;
         }
+    }
+
+    public function loginInProgress() {
+        if (OpenM_SessionController::get(self::LOGIN_IN_PROGRESS) !== true)
+            die("LOGIN not in progress");
+        $manager = OpenM_SSOClientPoolSessionManager::fromFile($this->config_file_path);
+        $sso = $manager->get(OpenM_SessionController::get(self::SESSION_API_SELECTED), false);
+        $sso->login(null, true);
+        OpenM_SessionController::set(self::LOGIN_IN_PROGRESS, false);
+        $this->return_to();
+    }
+
+    public function return_to() {
+        if (!isset($_GET[self::RETURN_TO_PARAMETER]))
+            die("return_to not found");
+        $return_to = $_GET[self::RETURN_TO_PARAMETER];
+        OpenM_Header::redirect(str_replace(self::DASH, "#", OpenM_URL::decode($return_to)));
     }
 
     public function isConnected($sso) {
@@ -117,5 +156,4 @@ class OpenM_SSOClientConnectionManagerServer {
     }
 
 }
-
 ?>
